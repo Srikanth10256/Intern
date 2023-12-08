8da08bd2edcb21fbf3eb46107300aa8fbf453c5c
Srikanth10256 <152681956+Srikanth10256@users.noreply.github.com>
2023-12-08 06:43:20 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int maxAttempts = 10;
+        int totalRounds = 3;
+        int totalScore = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+
+        for (int round = 1; round <= totalRounds; round++) {
+            int targetNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            int attempts = 0;
+
+            System.out.println("\nRound " + round + ": Guess the number between " + lowerBound + " and " + upperBound);
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == targetNumber) {
+                    System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+                    int roundScore = maxAttempts - attempts + 1;
+                    totalScore += roundScore;
+                    System.out.println("Round Score: " + roundScore);
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+
+                if (attempts == maxAttempts) {
+                    System.out.println("Sorry, you've reached the maximum number of attempts. The correct number was: " + targetNumber);
+                }
+            }
+        }
+
+        System.out.println("\nGame Over!");
+        System.out.println("Total Score: " + totalScore);
+
+        scanner.close();
+    }
+}
+
